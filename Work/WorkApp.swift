3e1c934b9fe0992d733ec0824e526841d9efd70b
daniel jermaine <danieljermaine1@gmail.com>
2024-01-17 07:51:21 +0100

    Initial Commit

---
+++ Work/WorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkApp.swift
+//  Work
+//
+//  Created by MacBook AIR on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
